16be72ac28fd11109a6da40e16681e5254b1dee4
TYEOLRIK\TyeolRik <kino6147@naver.com>
2019-04-09 22:26:53 +0900

    Initialize Source, balance.h, balance.obj.

--- FindFakeCoins/FindFakeCoins.cpp
+++ FindFakeCoins/FindFakeCoins.cpp
@@ -1,9 +1,14 @@
-﻿
-
-#include "pch.h"
+﻿#include "pch.h"
+#include "balance.h"
 #include <iostream>
+using namespace std;
+int main() {
+	int a[5] = { 0,1,2,3,-1 };
+	int b[6] = { 4,5,6,7,8,-1 };
+	int c[10] = { 10, 11, 12, 13, 14, 15, 16, 17, 18, -1 };
 
-int main()
-{
-    std::cout << "Hello World!\n"; 
+	initialize(33); // 불량 동전의 확률 p = 33%
+	int ret = balance(a, b, c);
+	cout << ret << endl;
+	return 0;
 }
